0fc36b1a2d7bca0a704429ce067f197f93bb6bc6
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-08-15 22:10:30 +0200

    cache whl in repo

--- toolkit/rkllm_toolkit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://raw.githubusercontent.com/airockchip/rknn-llm/release-v1.0.1/rkllm-toolkit/packages/rkllm_toolkit-1.0.1-cp38-cp38-linux_x86_64.whl
-pip install --no-cache-dir rkllm_toolkit-1.0.1-cp38-cp38-linux_x86_64.whl
-rm rkllm_toolkit-1.0.1-cp38-cp38-linux_x86_64.whl
